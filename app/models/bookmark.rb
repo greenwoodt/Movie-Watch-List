48e8bc579f53e24593726037307adf7670d0b046
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-08-18 13:24:38 +0200

    validations complete

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   validates :title, :overview, presence: true
+  has_many :bookmarks
 end
