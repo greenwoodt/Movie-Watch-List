d7fa3bcfd3cc12eb4e2e361fa9d8de2a703a2516
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-11-09 15:35:29 +0100

    container items alignment to list show

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,41 +28,6 @@ JSON.parse(user_serialized)["results"].each do |movie|
 end
 puts "Your Movies have been created!!"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# 10.times do |i|
-#   puts "Importing movies from page #{i + 1}"
-#   movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
-#   movies.each do |movie|
-#     puts "Creating #{movie['title']}"
-#     base_poster_url = "https://image.tmdb.org/t/p/original"
-#     Movie.create(
-#       title: movie['title'],
-#       overview: movie['overview'],
-#       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-#       rating: movie['vote_average']
-#     )
-#   end
-# end
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
