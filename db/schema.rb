2b938aba3f2b6fd80a3f5e65e0094a058d7be8c7
Tom Greenwood <tom.t.greenwood@gmail.com>
2023-01-19 15:24:48 +0100

    almost

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_095913) do
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.bigint "blob_id", null: falseco
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
